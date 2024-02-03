4b17a0cc37f7d0660d23aa1a9be6f41c20975d52
abed-a-78 <abed.re.k00021@gmail.com>
2024-02-03 09:20:39 -0800

    edit-typephone

--- src/constants/inputs.js
+++ src/constants/inputs.js
@@ -5,7 +5,7 @@ const inputs = [
     { name: "name", type: "text", placeholder: "Name" },
     { name: "lastName", type: "text", placeholder: "Last Name" },
     { name: "email", type: "email", placeholder: "Email" },
-    { name: "phone", type: "number", placeholder: "Phone" }
+    { name: "phone", type: "tel", placeholder: "Phone" }
 ];
 
 export default inputs;
